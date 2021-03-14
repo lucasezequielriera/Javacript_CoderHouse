e7d6a218858322ae64e4213cb7f74b5fd82bd3ef
Lucas Riera <lucasezequielriera@hotmail.com>
2021-03-14 14:08:25 -0300

    Iteraciones con "for; if; else if y break"

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,4 +1,32 @@
-let nombre = prompt("¡Buenas! Cuál es tu nombre?");
+/*let nombre = prompt("¡Buenas! Cuál es tu nombre?");
+
+if (nombre) {
+    ciudad = prompt("Perfecto " + nombre + ", de donde sos?");
+}
+else {
+    alert("Es necesario escribir un nombre");
+}
+
+if (ciudad) {
+    edad = prompt("Muy bien " + nombre + ", ahora dime, Cuál es tu edad?");
+}
+else {
+    alert("Debes indicar una ciudad");
+}
+
+if (edad) {
+    altura = prompt("Perfecto " + nombre + "! Ahora indicanos tu altura");
+}
+else {
+    alert("No es una altura válida");
+}
+
+if (edad >= 18 && altura >= 180) {
+    alert("AUTHORIZED ACCESS");
+} 
+else {
+    alert("ACCESS DENIED");
+}
 
 if (nombre) {
     alert("Genial, ahora... ") 
@@ -63,3 +91,41 @@ else if (edad > 40) {
 else if ((nombre && apellido) && (pais == "Argentina" && ciudad == "Buenos Aires") && (edad >= 18 <= 40)) {
     alert("Perfect! Ya validamos todos tus datos y estás seleccionado para entrar a nuestra zona VIP");
 }
+*/
+
+
+// Ciclos / Iteraciones //
+
+alert("Bienvenido! Para ganar tienes que elegir un número del 1 al 10 y que éste coincida con el número aleatorio que generará la PC, estás listo/a?");
+let conteo = 0;
+
+for(let i = 1; i <= 5; i++) {
+    let numero_aleatorio = (Math.floor(Math.random() * 10 + 1));
+    let numero_elegido = Number(prompt("Elegí un número:"));
+
+    if (numero_elegido === numero_aleatorio) {
+        alert(`GANASTE AL INTENTO Nº${i} !!!`);
+        break;
+    }
+    else if (numero_elegido < numero_aleatorio && numero_elegido != 0) {
+        alert(`FALLASTE`);
+        console.log(i)
+    }
+    else if (numero_elegido === 0){
+        alert("Esto no es un número, has perdido un turno");
+    }
+    else if (numero_elegido > numero_aleatorio && numero_elegido != 0) {
+        alert(`FALLASTE`);
+    }
+    if (i == 4) {
+        alert("Te queda un último intento!");
+        }
+    else if (i == 5) {
+        alert("Superaste el número de intentos posibles :(");
+    }
+}
+conteo = conteo + 1;
+console.log(`El número de intentos fué de: ${conteo}`);
+
+
+// Faltaría agregar la función de repetir que aún no la vimos, y por lo que busqué, se utiliza como impresión de una parte del "for"
